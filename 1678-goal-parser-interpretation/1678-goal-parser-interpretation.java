1c401cfabac778a11abd6e19f4d1862d00ea389d
Trinadh-dev <157801389+Trinadh-dev@users.noreply.github.com>
2024-04-12 23:26:46 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (70.77%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,6 +1,22 @@
 class Solution {
     public String interpret(String command) {
-        return command.replace("()","o").replace("(al)","al");
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+
+        while (index < command.length()) {
+            if (command.charAt(index) == 'G') {
+                result.append('G');
+                index++;
+            } else if (command.charAt(index) == '(' && command.charAt(index + 1) == ')') {
+                result.append('o');
+                index += 2;
+            } else {
+                result.append("al");
+                index += 4;
+            }
+        }
+
+        return result.toString();
          
     }
 }
